3242769965c54fbc3d40cf509398fb12da5f8f9b
Thomas <tbsaxophone@gmail.com>
2024-07-09 17:51:13 -0400

    Near final, just need to double check everything.

--- ChessAnalysis/server.R
+++ ChessAnalysis/server.R
@@ -12,12 +12,12 @@ queryChess <- function(category,chessType,username,year,month,chessTitle="All",s
     data <- GET(URL)
     parsed <- fromJSON(rawToChar(data$content))
     if(chessType=="All"){
-      chessInfo1 <- as_tibble(parsed$live_rapid) |> mutate(Type="Rapid")
-      chessInfo2 <- as_tibble(parsed$live_blitz)|> mutate(Type="Blitz")
-      chessInfo3 <- as_tibble(parsed$live_bullet)|> mutate(Type="Bullet")
+      chessInfo1 <- as_tibble(parsed$live_rapid) |> mutate(type="Rapid")
+      chessInfo2 <- as_tibble(parsed$live_blitz)|> mutate(type="Blitz")
+      chessInfo3 <- as_tibble(parsed$live_bullet)|> mutate(type="Bullet")
       chessInfo <- bind_rows(chessInfo1,chessInfo2,chessInfo3) |>
         mutate(country=substr(country,nchar(country)-1,nchar(country))) |>
-        select(username,score,rank,country,title,win_count,loss_count,draw_count,Type)
+        select(username,score,rank,country,title,win_count,loss_count,draw_count,type)
     if(chessTitle!="All"){
       chessInfo <- chessInfo |> filter(title==chessTitle)
     }
@@ -59,30 +59,30 @@ queryChess <- function(category,chessType,username,year,month,chessTitle="All",s
     data <- GET(URL)
     parsed <- fromJSON(rawToChar(data$content))
     if(chessType=="All"){
-      chessInfo1 <- as_tibble_row(unlist(parsed$chess_rapid)) |> mutate(Type="Rapid")
-      chessInfo2 <- as_tibble_row(unlist(parsed$chess_blitz)) |> mutate(Type="Blitz")
-      chessInfo3 <- as_tibble_row(unlist(parsed$chess_bullet))|> mutate(Type="Bullet") 
+      chessInfo1 <- as_tibble_row(unlist(parsed$chess_rapid)) |> mutate(type="Rapid")
+      chessInfo2 <- as_tibble_row(unlist(parsed$chess_blitz)) |> mutate(type="Blitz")
+      chessInfo3 <- as_tibble_row(unlist(parsed$chess_bullet))|> mutate(type="Bullet") 
       chessInfo <- bind_rows(chessInfo1,chessInfo2,chessInfo3) |>
         mutate(last.rating= as.integer(last.rating),last.date= as.integer(last.date),last.rd= as.integer(last.rd),best.rating= as.integer(best.rating),best.date= as.integer(best.date),record.win= as.integer(record.win),record.loss= as.integer(record.loss),record.draw= as.integer(record.draw)) |>
-        select(best.rating,record.win,record.loss,record.draw,Type)
+        select(best.rating,record.win,record.loss,record.draw,type)
       chessInfo
     }
     else if(chessType=="Rapid"){
       chessInfo <- as_tibble_row(unlist(parsed$chess_rapid)) |>
-        mutate(Type="Rapid",last.rating= as.integer(last.rating),last.date= as.integer(last.date),last.rd= as.integer(last.rd),best.rating= as.integer(best.rating),best.date= as.integer(best.date),record.win= as.integer(record.win),record.loss= as.integer(record.loss),record.draw= as.integer(record.draw)) |>
-        select(best.rating,record.win,record.loss,record.draw,Type)
+        mutate(type="Rapid",last.rating= as.integer(last.rating),last.date= as.integer(last.date),last.rd= as.integer(last.rd),best.rating= as.integer(best.rating),best.date= as.integer(best.date),record.win= as.integer(record.win),record.loss= as.integer(record.loss),record.draw= as.integer(record.draw)) |>
+        select(best.rating,record.win,record.loss,record.draw,type)
       chessInfo  
     }
     else if(chessType=="Blitz"){
       chessInfo <- as_tibble_row(unlist(parsed$chess_blitz)) |>
-        mutate(Type="Blitz",last.rating= as.integer(last.rating),last.date= as.integer(last.date),last.rd= as.integer(last.rd),best.rating= as.integer(best.rating),best.date= as.integer(best.date),record.win= as.integer(record.win),record.loss= as.integer(record.loss),record.draw= as.integer(record.draw)) |>
-        select(best.rating,record.win,record.loss,record.draw,Type)
+        mutate(type="Blitz",last.rating= as.integer(last.rating),last.date= as.integer(last.date),last.rd= as.integer(last.rd),best.rating= as.integer(best.rating),best.date= as.integer(best.date),record.win= as.integer(record.win),record.loss= as.integer(record.loss),record.draw= as.integer(record.draw)) |>
+        select(best.rating,record.win,record.loss,record.draw,type)
       chessInfo  
     }
     else if(chessType=="Bullet"){
       chessInfo <- as_tibble_row(unlist(parsed$chess_bullet)) |>
-        mutate(Type="Bullet",last.rating= as.integer(last.rating),last.date= as.integer(last.date),last.rd= as.integer(last.rd),best.rating= as.integer(best.rating),best.date= as.integer(best.date),record.win= as.integer(record.win),record.loss= as.integer(record.loss),record.draw= as.integer(record.draw)) |>
-        select(best.rating,record.win,record.loss,record.draw,Type)
+        mutate(type="Bullet",last.rating= as.integer(last.rating),last.date= as.integer(last.date),last.rd= as.integer(last.rd),best.rating= as.integer(best.rating),best.date= as.integer(best.date),record.win= as.integer(record.win),record.loss= as.integer(record.loss),record.draw= as.integer(record.draw)) |>
+        select(best.rating,record.win,record.loss,record.draw,type)
       chessInfo  
     }
     else {
@@ -100,10 +100,10 @@ queryChess <- function(category,chessType,username,year,month,chessTitle="All",s
     chessInfo <- as_tibble(parsed$games) 
     if (nrow(chessInfo) != 0){
       chessInfo <- chessInfo |>
-        mutate(Type=time_class) |>
+        mutate(type=time_class,end_time=as.Date(as.POSIXct(end_time, origin="1970-01-01"))) |>
         mutate(white.user=white$username,black.user=black$username,white.rating=white$rating,black.rating=black$rating,white.result=white$result,black.result=black$result)
       if(chessType != "All") {
-        chessInfo <- chessInfo |> filter(Type==tolower(chessType))
+        chessInfo <- chessInfo |> filter(type==tolower(chessType))
       }
       chessInfo
     }else chessInfo <- tibble()
@@ -120,7 +120,7 @@ function(input, output, session) {
   
   output$table2 <- renderDataTable({
     queryChess(category = "player", chessType = input$gameType3, username = input$user3, year = input$year3, month = input$month3, subcategory = "games") |>
-      select(white.user:black.result)
+      select(white.user:black.result,end_time,time_class)
   })
   
   output$table3 <- renderDataTable({
@@ -143,7 +143,7 @@ function(input, output, session) {
     filename="UserGames.csv",
     content=function(file){
       write.csv(queryChess(category = "player", chessType = input$gameType3, username = input$user3, year = input$year3, month = input$month3, subcategory = "games") |>
-                  select(white.user:black.result,time_class),file,row.names=FALSE)
+                  select(white.user:black.result,end_time,time_class),file,row.names=FALSE)
     })
   
   g  <- reactive({g <- ggplot(queryChess(category = "player", chessType =input$gameType2, username = input$user2, subcategory = "stats") |> 
@@ -151,9 +151,9 @@ function(input, output, session) {
     pivot_longer(cols=c(Win,Loss,Draw),names_to = "Record") |>
     mutate(Record = as_factor(Record)),aes(x=Record,y=value))})
   
-  output$table6 <- renderTable({queryChess(category = "player", chessType =input$gameType2, username = input$user2, subcategory = "stats") |> 
+  output$table6 <- renderTable({queryChess(category = "player", chessType ="All", username = input$user2, subcategory = "stats") |> 
     rename(Win=record.win,Loss=record.loss,Draw=record.draw) |>
-    select(Type,Win,Loss,Draw)
+    select(type,Win,Loss,Draw)
   })
   
   output$plot1 <- renderPlot(
@@ -182,29 +182,29 @@ function(input, output, session) {
    
   j <- reactive({i <- ggplot(queryChess(category = "player", chessType =input$gameType3, username = input$user3, year =input$year3, month = input$month3, subcategory = "games")|>
       mutate(userSide = ifelse(white.user!="tolkienatic","White","Black")) |>
-      mutate(userELO = ifelse(userSide=="White",white.rating,black.rating), oppELO = ifelse(userSide!="White",white.rating,black.rating),end_time=as.Date(as.POSIXct(end_time, origin="1970-01-01"))) |>
+      mutate(userELO = ifelse(userSide=="White",white.rating,black.rating), oppELO = ifelse(userSide!="White",white.rating,black.rating)) |>
       select(userELO,oppELO,end_time) |>
       group_by(end_time) |>
       summarize(meanuserELO = mean(userELO),meanoppELO=mean(oppELO)),aes(x=end_time))}) 
   
   output$plot4 <- renderPlot(
-    j()+geom_line(aes(y=meanuserELO,color="User")) + geom_line(aes(y=meanoppELO,color="Opponent"))+labs(x="Date",y="ELO",colour="Player")
+    j()+geom_line(aes(y=meanuserELO,color="User")) + geom_line(aes(y=meanoppELO,color="Opponent"))+labs(x="Date",y="ELO",colour="Player",title="Comparison of ELO Fluctuation of Player and Opponent")+theme(plot.title = element_text(hjust=0.5))
   )
   
   output$table5 <- renderTable(
     if(input$freqVar1=="title"){
       queryChess(category = "leaderboards", chessType ="All",chessTitle="All")|>
-      select(Type,title) |>
+      select(type,title) |>
       filter(!is.na(title)) |>
-      group_by(Type,title) |>
+      group_by(type,title) |>
       summarize(count=n()) |>
       pivot_wider(names_from=title,values_from = count) |>
       mutate(across(everything(),~replace_na(.,0)))
     }
     else if (input$freqVar1=="country"){
       queryChess(category = "leaderboards", chessType ="All",chessTitle="All")|>
-        select(Type,country) |>
-        group_by(Type,country) |>
+        select(type,country) |>
+        group_by(type,country) |>
         filter(!is.na(country)) |>
         summarize(count=n()) |>
         pivot_wider(names_from=country,values_from = count) |>
@@ -213,23 +213,75 @@ function(input, output, session) {
   )
   
   output$table7 <- renderTable({
-    queryChess(category = "player", chessType =input$gameType3, username = input$user3, year =input$year3, month = input$month3, subcategory = "games") |>
-      mutate(userSide = ifelse(white.user=="tolkienatic","White","Black")) |>
-      mutate(userResult = ifelse(userSide=="White",white.result,black.result)) |>
-      mutate(userResult = ifelse(userResult=="win","Win",
-                                 ifelse(userResult=="agreed","Draw",
-                                        ifelse(userResult=="stalemate","Draw",
-                                               ifelse(userResult=="insufficient","Draw",
-                                                      ifelse(userResult=="50move","Draw",
-                                                             "Loss")))))) |>
-      group_by(userSide,userResult) |>
-      select(userSide,userResult) |>
-      summarize(count=n()) |>
-      pivot_wider(names_from = userResult,values_from = count) |>
-      select(userSide,Win,Loss,Draw) |>
-      arrange(desc(userSide))
+    if(input$freqVar2=="side"){
+      queryChess(category = "player", chessType ="All", username = input$user3, year =input$year3, month = input$month3, subcategory = "games") |>
+        mutate(userSide = ifelse(white.user==input$user3,"White","Black")) |>
+        mutate(userResult = ifelse(userSide=="White",white.result,black.result)) |>
+        mutate(userResult = ifelse(userResult=="win","Win",
+                                   ifelse(userResult=="agreed","Draw",
+                                          ifelse(userResult=="stalemate","Draw",
+                                                 ifelse(userResult=="insufficient","Draw",
+                                                        ifelse(userResult=="50move","Draw",
+                                                               "Loss")))))) |>
+        group_by(type,userSide) |>
+        select(type,userSide) |>
+        summarize(count=n()) |>
+        pivot_wider(names_from = userSide,values_from = count) |>
+        select(type,White,Black) |>
+        arrange(desc(type))|>
+        mutate(across(everything(),~replace_na(.,0)))
+    }
+    else if (input$freqVar2=="outcome"){
+      queryChess(category = "player", chessType ="All", username = input$user3, year =input$year3, month = input$month3, subcategory = "games") |>
+        mutate(userSide = ifelse(white.user==input$user3,"White","Black")) |>
+        mutate(userResult = ifelse(userSide=="White",white.result,black.result)) |>
+        mutate(userResult = ifelse(userResult=="win","Win",
+                                   ifelse(userResult=="agreed","Draw",
+                                          ifelse(userResult=="stalemate","Draw",
+                                                 ifelse(userResult=="insufficient","Draw",
+                                                        ifelse(userResult=="50move","Draw",
+                                                               "Loss")))))) |>
+        group_by(type,userResult) |>
+        select(userSide,userResult) |>
+        summarize(count=n()) |>
+        pivot_wider(names_from = userResult,values_from = count) |>
+        select(type,Win,Loss,Draw) |>
+        arrange(desc(type)) |>
+        mutate(across(everything(),~replace_na(.,0)))
+    }
   })
   
+  output$table8 <- renderTable({
+    if(input$statVar1=="User"){
+      queryChess(category = "player", chessType ="All", username = input$user3, year =input$year3, month = input$month3, subcategory = "games") |>
+      mutate(Side = ifelse(white.user==input$user3,"White","Black")) |>
+      mutate(userELO = ifelse(Side=="White",white.rating,black.rating)) |>
+      select(Side,userELO) |>
+      group_by(Side) |>
+      summarize(meanELO = mean(userELO),sdELO = sd(userELO),medianELO = median(userELO),iqrELO=IQR(userELO)) |>
+      arrange(desc(Side)) |>
+      mutate(Side = factor(Side))
+    }
+    else if (input$statVar1=="Opponent"){
+      queryChess(category = "player", chessType ="All", username = input$user3, year =input$year3, month = input$month3, subcategory = "games") |>
+        mutate(Side = ifelse(white.user!=input$user3,"White","Black")) |>
+        mutate(userELO = ifelse(Side=="White",white.rating,black.rating)) |>
+        select(Side,userELO) |>
+        group_by(Side) |>
+        summarize(meanELO = mean(userELO),sdELO = sd(userELO),medianELO = median(userELO),iqrELO=IQR(userELO)) |>
+        arrange(desc(Side)) |>
+        mutate(Side = factor(Side))
+    }
+  })
+ 
+ k <- reactive({
+    k <- ggplot(queryChess(category = "leaderboards", chessType ="All",chessTitle="All") |>
+                  select(score,rank,type,country,title) |>
+                  filter(!is.na(title)),aes(x=rank,y=score,color=get(input$facetVal1)))
+  })
+  output$plot5 <- renderPlot({
+    k()+geom_jitter()+facet_wrap(~get(input$facetVal1))+ labs(x="Leaderboard Rankings",y="ELO Ranking",color=input$facetVal1)+theme(plot.title = element_text(hjust=0.5))
+  })
 } 
   
 
